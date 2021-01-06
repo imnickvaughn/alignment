f4c750cb171b5046d1f152bfe3e20feb8bd1226f
Nicholas Vaughn <vaughnick@gmail.com>
2021-01-06 15:02:06 -0500

    resolved naming bug, building fine now

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
-import HeaderComponent from './header.component.js'
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 import FormComponent from './form.component.js';
 import Link from 'next/link';
 import React from 'react';
 import FaviconComponent from './favicon.component.js';
 import ResultsComponent from './results.component.js';
-import styles from '../styles/root.module.css'
+import styles from '../styles/home.module.css'
+import HeaderComponent from './header.component.js';
 
 export default function Home() {
   const title = "Molecular Alignment Tool"
